184119e3ebda3d86f055967c7f7467e282def177
Christian Segovia <christianfs@gmail.com>
2022-12-01 22:30:04 -0500

    First commit
    Created the services of our microservices architecture, without code,
    just the template. API Gateway and Server Discovery have also been
    implemented and are working.

---
+++ prixbanque/transfer-service/src/main/java/com/prixbanque/transferservice/TransferServiceApplication.java
@@ -0,0 +1,14 @@
+package com.prixbanque.transferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class TransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferServiceApplication.class, args);
+    }
+}
