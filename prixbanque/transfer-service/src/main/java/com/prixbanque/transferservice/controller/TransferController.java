d64c894c97c3bc7688466cd65d601a44a93f021c
Christian Segovia <christianfs@gmail.com>
2022-12-05 21:03:17 -0500

    Update endpoints

--- prixbanque/transfer-service/src/main/java/com/prixbanque/transferservice/controller/TransferController.java
+++ prixbanque/transfer-service/src/main/java/com/prixbanque/transferservice/controller/TransferController.java
@@ -11,24 +11,25 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/transfer")
 @RequiredArgsConstructor
 public class TransferController {
 
     private final TransferService transferService;
 
-    @PostMapping(path = "/api/transfer")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createTransfer(@RequestBody TransferRequest transferRequest) {
         transferService.createTransfer(transferRequest);
     }
 
-    @PostMapping(path = "/api/transfer/commit")
+    @PostMapping(path = "/commit")
     @ResponseStatus(HttpStatus.OK)
     public Boolean commitTransfer(@RequestParam(name = "confirmationKey") UUID confirmationKey, @RequestParam(name = "recipientsEmail") String recipientsEmail) {
         return transferService.commitTransfer(confirmationKey, recipientsEmail);
     }
 
-    @GetMapping(path = "/api/transfer/account/{accountNumber}")
+    @GetMapping(path = "/account/{accountNumber}")
     @ResponseStatus(HttpStatus.OK)
     public List<TransferResponse> getAllTransfersByAccountNumber(@PathVariable String accountNumber) {
         return transferService.getAllTransfersByAccountNumber(accountNumber);
